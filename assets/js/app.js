7697d39fbf34a044d4fea05d66e2a14299ce44f1
Nat Morán <nath.moran.v@gmail.com>
2022-06-23 19:19:00 -0500

    jalando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,71 +1,88 @@
-console.log(`ola mundo`)
+console.log(`ola mundo`);
 
 const video = document.querySelectorAll(`.video`);
 
-    for (let i = 0; i < video.length; i++) {
-      video[i].addEventListener(`mouseenter`,
-    function(e){
-        video[i].play()
-    })
-    video[i].addEventListener(`mouseleave`,
-    function(e){
-        video[i].pause()
-    })
+for (let i = 0; i < video.length; i++) {
+  video[i].addEventListener(`mouseenter`, function (e) {
+    video[i].play();
+  });
+  video[i].addEventListener(`mouseleave`, function (e) {
+    video[i].pause();
+  });
 }
 
-const pokemonContainer = document.querySelector(`#gallery`)
+const pokemonContainer = document.querySelector(`#gallery`);
 
-function getPokemon(id){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then(response=> response.json())
-    .then(data => {
-        createPokemon(data);
-    })
-}
-
-function getPokemones(num){
-    for (let i = 1 ; i<= num ; i++){
-        getPokemon(i);
-    }
-}
-function createPokemon(pokemon) {
-    const card = document.createElement(`div`);
-    card.classList.add(`pokemon-card`);
-
-    const spriteContainer = document.createElement(`div`);
-    spriteContainer.classList.add(`img-container`);
-
-    const sprite = document.createElement(`img`);
-    sprite.src = pokemon.sprites.front_default;
-    sprite.classList.add(`sprite`);
-
-    const infoContainer = document.createElement(`div`);
-    infoContainer.classList.add(`info-container`);
-
-    const name = document.createElement(`p`);
-    name.classList.add(`name`);
-    name.textContent = pokemon.name;
-
-    const tipos = pokemon.types;
-    console.log(tipos)
-    
-    let tipo = "";
-    tipos.forEach(item => {
-        tipo += `<li><img class="type" src="../assets/images/types/${item.type.name}.png"</li>`
+function getPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      createPokemon(data);
     });
-    console.log(tipo)
-    const tipoPokemon = document.createElement(`ul`);
-    tipoPokemon.innerHTML = `${tipo}`;
-    
-    card.appendChild(spriteContainer);
-    spriteContainer.appendChild(sprite)
-    card.appendChild(infoContainer);
-    infoContainer.appendChild(name);
-    infoContainer.appendChild(tipoPokemon);
-
-    pokemonContainer.appendChild(card)
-
 }
 
-getPokemones(200)
+function getPokemones(num) {
+  for (let i = 1; i <= num; i++) {
+    getPokemon(i);
+  }
+}
 
+function createPokemon(pokemon) {
+
+  const card = document.createElement(`div`);
+  card.classList.add(`pokemon-card`);
+
+  const spriteContainer = document.createElement(`div`);
+  spriteContainer.classList.add(`img-container`);
+
+  const sprite = document.createElement(`img`);
+  sprite.src = pokemon.sprites.front_default;
+  sprite.classList.add(`sprite`);
+
+  const infoContainer = document.createElement(`div`);
+  infoContainer.classList.add(`info-container`);
+
+  const name = document.createElement(`p`);
+  name.classList.add(`name`);
+  name.textContent = pokemon.name;
+
+  const tipos = pokemon.types;
+
+  let tipo = "";
+  tipos.forEach((item) => {
+    tipo += `<li><img class="type" src="../assets/images/types/${item.type.name}.png"</li>`;
+  });
+
+  const tipoPokemon = document.createElement(`ul`);
+  tipoPokemon.innerHTML = `${tipo}`;
+
+  card.appendChild(spriteContainer);
+  spriteContainer.appendChild(sprite);
+  card.appendChild(infoContainer);
+  infoContainer.appendChild(name);
+  infoContainer.appendChild(tipoPokemon);
+
+  pokemonContainer.appendChild(card);
+
+  const modalImage = document.querySelector(".modal_img")
+  modalImage.src = pokemon.sprites.front_default;
+
+  const modalTitle = document.querySelector(".modal_title")
+  modalTitle.textContent = pokemon.name
+
+  const modal = document.querySelector(".modal");
+  
+  const closeModal = document.querySelector(".modal_close");
+
+  card.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.add("modal-show");
+  });
+
+  closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.remove("modal-show");
+  });
+}
+
+getPokemones(200);
